a33478d449a0b6f5ea8aae247caab95b5ab66c27
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2022-12-17 22:16:17 +0300

    Game Options started

--- Assets/_Scripts/Start Menu/Datas.cs
+++ Assets/_Scripts/Start Menu/Datas.cs
@@ -2,19 +2,58 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+
+public enum GameDifficulity
+{
+    EASY,
+    MİD,
+    HARD
+}
 
 public class Datas : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown difficulity;
     [SerializeField] private TMP_InputField rows;
     [SerializeField] private TMP_InputField colums;
-    
-    
+
+    private int rowSize;
+    private int columSize;
+    private GameDifficulity gameDifficulity;
+
     private void Update()
     {
-        Debug.Log(difficulity.options[difficulity.value].text);
+        
+    }
+
+    private bool IsValueEntered(TMP_InputField inputField)
+    {
+        return rows.text != "";
+    }
+
+    public void StartGame()
+    {
+        if (IsValueEntered(rows))
+        {
+            rowSize = Convert.ToInt32(rows.text);
+        }
+        else
+        {
+            rowSize = Convert.ToInt32(rows.placeholder.GetComponent<TMP_Text>().text);
+        }
+        
+        if (IsValueEntered(colums))
+        {
+            columSize = Convert.ToInt32(colums.text);
+        }
+        else
+        {
+            columSize = Convert.ToInt32(colums.placeholder.GetComponent<TMP_Text>().text);
+        }
+        
+        
     }
 }
