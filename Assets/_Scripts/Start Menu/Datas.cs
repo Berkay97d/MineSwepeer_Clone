6d653253fd18d03a34df3a0a329177a320cf9254
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2022-12-22 03:00:46 +0300

    Data System Completed
    FADE OUT ILE SAHNE GECISI YAPILMASI KALDI
    GAME AREA CLAMPLERI DEGER DEGISTIĞINDE EVENTINE BAĞLANABİLİR

--- Assets/_Scripts/Start Menu/Datas.cs
+++ Assets/_Scripts/Start Menu/Datas.cs
@@ -16,44 +16,94 @@ public enum GameDifficulity
 
 public class Datas : MonoBehaviour
 {
+    [Header("UI REFERENCES")]
     [SerializeField] private TMP_Dropdown difficulity;
     [SerializeField] private TMP_InputField rows;
     [SerializeField] private TMP_InputField colums;
-
+    
     private int rowSize;
     private int columSize;
+    
+    private readonly int maxSize = 30;
+    private readonly int minSize = 4;
+    
     private GameDifficulity gameDifficulity;
-
-    private void Update()
+    
+    private struct GameData
     {
-        
+        public int RowSize;
+        public int ColumSize;
+        public GameDifficulity GameDifficulity;
     }
 
-    private bool IsValueEntered(TMP_InputField inputField)
-    {
-        return rows.text != "";
-    }
-
+    
+    
     public void StartGame()
+    {
+        CheckGameDatas();
+        Debug.Log("Row size: " + rowSize);
+        Debug.Log("Colum size: " + columSize);
+        Debug.Log("Difficultiy size: " + gameDifficulity);
+    }
+    
+    private GameData CheckGameDatas()
+    {
+        GameData gameData = new GameData();
+        gameData.RowSize = RowSizeData();
+        gameData.ColumSize = ColumSizeData();
+        gameData.GameDifficulity = DifficulityData();
+
+        return gameData;
+    }
+    
+    private int RowSizeData()
     {
         if (IsValueEntered(rows))
         {
-            rowSize = Convert.ToInt32(rows.text);
+            rowSize = Mathf.Clamp(Convert.ToInt32(rows.text), minSize, maxSize); //Convert.ToInt32(rows.text);
+            rows.text = rowSize.ToString();
         }
         else
         {
             rowSize = Convert.ToInt32(rows.placeholder.GetComponent<TMP_Text>().text);
         }
         
+        return rowSize;
+    }
+    
+    private int ColumSizeData()
+    {
         if (IsValueEntered(colums))
         {
-            columSize = Convert.ToInt32(colums.text);
+            columSize = Mathf.Clamp(Convert.ToInt32(colums.text), minSize, maxSize); //Convert.ToInt32(colums.text);
+            colums.text = columSize.ToString();
         }
         else
         {
             columSize = Convert.ToInt32(colums.placeholder.GetComponent<TMP_Text>().text);
         }
         
-        
+        return columSize;
     }
+
+    private GameDifficulity DifficulityData()
+    {
+        var selectedDif = difficulity.value;
+
+        gameDifficulity = selectedDif switch
+        {
+            0 => GameDifficulity.EASY,
+            1 => GameDifficulity.MİD,
+            _ => GameDifficulity.HARD
+        };
+
+        return gameDifficulity;
+    }
+    
+    private bool IsValueEntered(TMP_InputField inputField)
+    {
+        return inputField.text != "";
+    }
+    
+    
 }
